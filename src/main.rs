edbe2a1135849d851cfd36125de10fd846d33be4
Brice Duke <bricewduke@gmail.com>
2021-05-30 22:10:54 -0500

    hello world

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use::actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello Rust!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Yo!")
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    }).bind("127.0.0.1:1337")?.run().await
+}
